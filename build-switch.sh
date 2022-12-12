90f25c532eb2d10f0f7d58f497c6bff8d1490fb8
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2022-12-12 02:54:08 +0100

    Replace romfs with sdcard folder

--- build-switch.sh
+++ build-switch.sh
@@ -4,12 +4,16 @@ if [ -d ./build ]; then
     rm -rf ./build
 fi
 
-mkdir -p ./build
+mkdir -p ./build ./out
+
+INSTALL_DIR="${PWD}/out"
 
 cd ./build
 
-cmake -DCMAKE_TOOLCHAIN_FILE="${DEVKITPRO}/cmake/Switch.cmake" ..
+cmake -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" -DCMAKE_TOOLCHAIN_FILE="${DEVKITPRO}/cmake/Switch.cmake" ..
 
 make -j$(nproc)
+
+make install
 
 cd ..
